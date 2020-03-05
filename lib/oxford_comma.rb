ad69e463383acee6cf3a0f25e1cebff06f3a7d90
James Dodds <dodds1990@gmail.com>
2020-03-05 21:59:49 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,7 @@ def oxford_comma(array)
     return array.join
   elsif array.length == 2
     return array.join(" and ")
-  elsif array.length == 3
+  else
     last_item = array[-1]
     array.pop
     output = array.join(", ")
